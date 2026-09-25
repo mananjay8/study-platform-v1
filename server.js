4d44cd69b026a0974cff2328261a5a0aa7f377a5
mananjay8 <golg8887@gmail.com>
2026-09-25 08:20:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 
-const app = express();app.use((req, res, next) => {
+const app = express();
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
